0a4347d53797f553edf639a83f4edf115b506b78
Nupoor Shetye <shetyenupoor10@gmail.com>
2023-12-21 00:47:40 +0530

    Implementing object detection and flash cards functionality

--- src/components/FlashCards/FlashCards.jsx
+++ src/components/FlashCards/FlashCards.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import './FlashCards.css'
+import React, { useState} from 'react';
+import ObjectDetector from './ObjectDetector';
+import CardsContainer from './CardsContainer';
 
 const FlashCards = () => {
+
+  const [ activeTab, setActiveTab ] = useState('cards');
+
+  const showCardsHandle = () => {
+    setActiveTab('cards')
+  }
+
+  const addCardsHandle = () => {
+    setActiveTab('cam')
+  }
+
   return (
-    <div>FlashCards</div>
+    <div className='cards__page__wrapper'>
+      <h1>Flash Cards</h1>
+      <div className='tabs__container'>
+        <button onClick={showCardsHandle} className='secondary__btn'>FLASHCARDS</button>
+        <button onClick={addCardsHandle} className='secondary__btn'>NEW CARD</button>
+      </div>
+      { activeTab === "cards" ? (
+        <CardsContainer />
+      ) : (
+        <ObjectDetector />
+      )}
+    </div>
   )
 }
 
