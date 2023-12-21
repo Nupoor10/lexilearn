6146148a2ffe8e04642a7ac4633520fde49b83ac
Nupoor Shetye <shetyenupoor10@gmail.com>
2023-12-21 11:33:30 +0530

    Implementing notes functionality

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import "./Notes.css"
+import React, { useState } from 'react';
+import NotesContainer from './NotesContainer';
+import NewNote from './NewNote';
+import "./Notes.css";
 
 const Notes = () => {
+  const [ activeTab, setActiveTab ] = useState('notes');
+
+  const addNoteHandle = () => {
+    setActiveTab('new')
+  }
+
+  const allNotesHandle = () => {
+    setActiveTab('notes')
+  }
+
   return (
-    <div>Notes</div>
+    <div className='notes__page__wrapper'>
+      <h3>Notes</h3>
+      <div className='notes__page__btn__wrapper'>
+        <button onClick={allNotesHandle} className='secondary__btn'>ALL NOTES</button>
+        <button onClick={addNoteHandle} className='secondary__btn'>NEW NOTE</button>
+      </div>
+      {activeTab === 'notes' ? 
+        ( <NotesContainer />) : 
+        ( <NewNote />)
+      }
+    </div>
   )
 }
 
